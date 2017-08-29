560c7c50002d6862b844d3f01886ac0a949dbbae
ah1at <t29ahmad@gmail.com>
2017-08-29 17:29:07 -0400

    first couple of exercises

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+  public static void main( String[] args ) {
+    System.out.println( "I, Talha Ahmad, am determined to learn how to code." );
+    System.out.println( "Today's date is August 29, 2017." );
+  }
+}
